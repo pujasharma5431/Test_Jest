31a67d3ca2fadca9d456fbca95ae31971d9c04ab
pujasharma5431 <pujasharma5431@gmail.com>
2023-04-21 09:19:39 +0545

    API test GET

---
+++ api.test.js
@@ -0,0 +1,15 @@
+const axios = require('axios');
+const baseUrl = "https://dummyjson.com/products";
+
+describe("GET /products", () => { 
+  for (let id = 1; id <= 5; id++) {
+    it(`should return category for product ${id}`, async () => {
+      const response = await axios.get(`${baseUrl}/${id}`);
+expect(response.status).toBe(200);
+      expect(response.data.category).toBe('smartphones');
+      expect(response.data.price).toBeGreaterThan(100);
+    });
+  }
+},
+)
+
